a3a20b070174036f32dd47bb1f70269f764a23a0
Ricardo Lopes Correa <sp.ricardo@gmail.com>
2018-08-06 23:37:27 -0300

    alteracoes

--- workflow/deploy_gcl.sh
+++ workflow/deploy_gcl.sh
@@ -4,6 +4,24 @@
 #git clone https://github.com/ricardolc/Exercicios.git
 #ksh
 
+
+echo "================================"
+echo "installing java default-jre"
+
+sudo apt-get install -y default-jre
+
+export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
+
+export PATH=$JAVA_HOME/bin:$PATH
+
+echo "================================"
+echo "installing java maven"
+
+sudo apt-get install -y maven
+
+echo "================================"
+echo "installing apache"
+
 sudo apt-get -y install apache2
 
 sudo a2enmod proxy
@@ -13,7 +31,11 @@ sudo a2enmod lbmethod_byrequests
 
 
 
-
+echo "================================"
+echo "copy 000-default.conf"
+sudo cp 000-default.conf /etc/apache2/sites-enabled 
 
 sudo /etc/init.d/apache2 restart
-java -jar demo-0.0.1-SNAPSHOT.jar
+
+
+#java -jar demo-0.0.1-SNAPSHOT.jar

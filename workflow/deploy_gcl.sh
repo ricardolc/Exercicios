5d21e9b06518f1b1f2e3e90b3349c00161c4d7b9
Ricardo Lopes Correa <sp.ricardo@gmail.com>
2018-08-09 21:48:53 -0300

    Update deploy_gcl.sh

--- workflow/deploy_gcl.sh
+++ workflow/deploy_gcl.sh
@@ -1,9 +1,9 @@
 #sudo apt-get update
-
 #sudo apt-get -y install git
 #git clone https://github.com/ricardolc/Exercicios.git
 #ksh
-
+#cd cd /home/sp_ricardo/Exercicios/workflow/autorization
+#sh deploy_gcl.sh
 
 echo "================================"
 echo "installing java default-jre"
@@ -29,19 +29,19 @@ sudo a2enmod proxy_http
 sudo a2enmod proxy_balancer
 sudo a2enmod lbmethod_byrequests
 
-
-
 echo "================================"
 echo "copy 000-default.conf"
 sudo cp 000-default.conf /etc/apache2/sites-enabled 
 
 sudo /etc/init.d/apache2 restart
 
-#cd /home/sp_ricardo/Exercicios/workflow/autorization
-#mvn clean install
+echo "================================"
+echo "app start"
 
+cd /home/sp_ricardo/Exercicios/workflow/autorization
+mvn clean install
 
-echo #start service
+echo "start app service"
 
 sudo ln -s /home/sp_ricardo/Exercicios/workflow/autorization/target/demo-0.0.1-SNAPSHOT.jar /etc/init.d/demo-0.0.1-SNAPSHOT	
 sudo update-rc.d demo-0.0.1-SNAPSHOT defaults

7a28ae4149f53573ef962a158bfea46487987108
Ricardo Lopes Correa <sp.ricardo@gmail.com>
2018-08-09 22:00:49 -0300

    Create deploy_gcl_2.sh

---
+++ workflow/deploy_gcl_2.sh
@@ -0,0 +1,14 @@
+
+echo "================================"
+echo "app start"
+
+cd /home/sp_ricardo/Exercicios/workflow/autorization
+mvn clean install
+
+echo "start app service"
+
+sudo ln -s /home/sp_ricardo/Exercicios/workflow/autorization/target/demo-0.0.1-SNAPSHOT.jar /etc/init.d/demo-0.0.1-SNAPSHOT	
+sudo update-rc.d demo-0.0.1-SNAPSHOT defaults
+sudo service demo-0.0.1-SNAPSHOT start
+
+#java -jar /home/sp_ricardo/Exercicios/workflow/autorization/target/demo-0.0.1-SNAPSHOT.jar
